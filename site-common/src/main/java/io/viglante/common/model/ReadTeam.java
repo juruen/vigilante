4d9c848c2c470b9379515dee9031b53dc94a70ce
Javier Uruen Val <javi.uruen@gmail.com>
2016-02-14 20:51:20 +0100

    Add ReadTeam

---
+++ site-common/src/main/java/io/viglante/common/model/ReadTeam.java
@@ -0,0 +1,26 @@
+package io.viglante.common.model;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Builder
+public class ReadTeam {
+    private final Optional<Long> id;
+    private final String name;
+    private final List<User> users;
+
+    @JsonCreator
+    public ReadTeam(@JsonProperty("id") Optional<Long> id, @JsonProperty("name") String name,
+                    @JsonProperty("users") List<User> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+    }
+}
